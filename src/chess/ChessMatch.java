132cbf2c528f72dd110e6c209575794b309a9677
maaxg <maxgps5@hotmail.com>
2020-01-09 14:15:05 -0300

    Piece move count

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -88,7 +88,8 @@ public class ChessMatch {
         }
     }
     private Piece makeMove(Position source, Position target){
-        Piece p = board.removePiece(source);
+        ChessPiece p = (ChessPiece) board.removePiece(source);
+        p.increaseMoveCount();
         Piece capturedPiece = board.removePiece(target);
         board.placePiece(p, target);
         if(capturedPiece != null){
@@ -98,7 +99,8 @@ public class ChessMatch {
         return capturedPiece;
     }
     private void undoMove(Position source, Position target, Piece capturedPiece){
-        Piece p = board.removePiece(target);
+        ChessPiece p = (ChessPiece)board.removePiece(target);
+        p.decreaseMoveCount();
         board.placePiece(p, source);
         if(capturedPiece != null){
             board.placePiece(capturedPiece, source);
